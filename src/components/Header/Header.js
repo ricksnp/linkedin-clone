a531e0a8ec6def33f122f1fb1cf4e73e3a69b978
Nathan Ricks <54324815+ricksnp@users.noreply.github.com>
2021-01-29 16:22:14 -0500

    Almost finished Header Component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import linked from '../../assets/linkedin.svg';
+import HeaderOption from '../HeaderOption/HeaderOption';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import nathan from '../../assets/Nathan_Ricks.jpg';
 
 
 function Header() {
     return (
         <div className='header'>
-            <h1>This is the header</h1>
             <div className="header__left">
-                <img src="" className="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt="" />
+                <img src={linked} alt="" />
                 <div className="header__search">
-                    {/* Search icon */}
+                    <SearchIcon />
                     <input type="text"></input>
                 </div>
             </div>
 
             <div className="header__right">
-
+                <HeaderOption Icon={HomeIcon} title='Home' />
+                <HeaderOption Icon={SupervisorAccountIcon} title='My Network' />
+                <HeaderOption Icon={BusinessCenterIcon} title='Jobs' />
+                <HeaderOption Icon={ChatIcon} title='Messaging' />
+                <HeaderOption Icon={NotificationsIcon} title='Notifications' />
+                <HeaderOption avatar={nathan} title='me' />
             </div>
         </div>
 
